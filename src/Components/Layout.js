344d60f2fb5561d124bd4ef0dca78fe2e63c1f64
Roderick Trice <rocky_trice@yahoo.com>
2019-02-04 13:49:57 -0500

    Added a footer component. re-used the logo compoenent and made it a clickable link

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header_footer/Header";
+import Footer from "./Header_footer/Footer";
 
 const Layout = props => {
   return (
     <div>
       <Header />
       {props.children}
+      <Footer />
     </div>
   );
 };
