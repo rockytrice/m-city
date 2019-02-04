8d2e4e0232140deeb2104c6c26ea66cfb4a2df5e
Roderick Trice <rocky_trice@yahoo.com>
2019-02-04 12:46:58 -0500

    creating the logic for the header component. Imported the AppBar, Toolbar, Button, from material-ui and added custom styles to the AppBar and Toolbar

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: "#98c5e9",
+          boxShadow: "none",
+          padding: "10px 0",
+          borderBottom: "2px solid #00285e"
+        }}
+      >
+        <Toolbar styles={{ display: "flex" }}>
+          <div styles={{ flexGrow: 1 }}>
+            <div className="header_logo">LOGO</div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    );
   }
 }
 
