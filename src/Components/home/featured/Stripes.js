c575ed85476e16c87f174db568fa4b3e438e4855
Roderick Trice <rocky_trice@yahoo.com>
2019-02-06 12:18:59 -0500

    Home: Finishing the featured animation of the number and text. used the same steps that was used to create the animation of the stripes for the number.

--- src/Components/home/featured/Stripes.js
+++ src/Components/home/featured/Stripes.js
@@ -51,7 +51,7 @@ class Stripes extends Component {
           left: [stripe.left],
           rotate: [stripe.rotate],
           top: [stripe.top],
-          timing: { delay: stripe.delay, duration: 200, ease: easePolyOut },
+          timing: { delay: stripe.delay, duration: 400, ease: easePolyOut },
           events: {
             end() {
               console.log("animation finished");
