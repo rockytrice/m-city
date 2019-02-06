3ab1471670d0536b8eb8d906e3dec357ae4443de
Roderick Trice <rocky_trice@yahoo.com>
2019-02-06 13:57:20 -0500

    Home: Featured Animation complete. finished adding the logic for animating the featured player and the text for the home section.

--- src/Components/home/featured/Text.js
+++ src/Components/home/featured/Text.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { easePolyOut } from "d3-ease";
 import Animate from "react-move/Animate";
-
+import FeaturedPlayer from "../../../Resources /images/featured_player.png";
 class Text extends Component {
+  //<==============================animates the Number 3======================================>
   animateNumber = () => (
     <Animate
       show={true}
@@ -31,8 +32,113 @@ class Text extends Component {
       }}
     </Animate>
   );
+
+  // <============================animates the first text "League"=================================>
+
+  animateFirstText = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 450
+      }}
+      enter={{
+        opacity: [1],
+        x: [273],
+        y: [450],
+        timing: { duration: 500, ease: easePolyOut }
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_first"
+            style={{
+              opacity,
+              x,
+              y,
+              transform: `translate(${x}px, ${y}px)`
+            }}
+          >
+            League
+          </div>
+        );
+      }}
+    </Animate>
+  );
+
+  // <========================animates the second text "championships"=============================>
+  animateSecondText = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 586
+      }}
+      enter={{
+        opacity: [1],
+        x: [273],
+        y: [586],
+        timing: { delay: 300, duration: 500, ease: easePolyOut }
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_second"
+            style={{
+              opacity,
+              x,
+              y,
+              transform: `translate(${x}px, ${y}px)`
+            }}
+          >
+            Championships
+          </div>
+        );
+      }}
+    </Animate>
+  );
+
+  //   <==========================animates the featured player=====================================>
+  animatePlayer = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0
+      }}
+      enter={{
+        opacity: [1],
+
+        timing: { delay: 800, duration: 500, ease: easePolyOut }
+      }}
+    >
+      {({ opacity }) => {
+        return (
+          <div
+            className="featured_player"
+            style={{
+              opacity,
+              background: `url(${FeaturedPlayer})`,
+              transform: `translate(684px,120px)`
+            }}
+          />
+        );
+      }}
+    </Animate>
+  );
+
   render() {
-    return <div className="featured_text">{this.animateNumber()}</div>;
+    return (
+      <div className="featured_text">
+        {this.animatePlayer()}
+        {this.animateNumber()}
+        {this.animateFirstText()}
+        {this.animateSecondText()}
+      </div>
+    );
   }
 }
 
