8077da770c871b9e6fe444daaa3fc9983c2d8961
Roderick Trice <rocky_trice@yahoo.com>
2019-02-11 22:52:56 -0500

    Created the Matches component which will be used as a resuable component for the see more matches link

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Featured from "./featured";
+import MachesHome from "./matches";
 
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured />
+      <MachesHome />
     </div>
   );
 };
---
+++ src/Components/home/matches/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Tag } from "../../utils/misc";
+const MatchesHome = () => {
+  return (
+    <div className="home_matches_wrapper">
+      <div className="container">
+        {/* TAG*/}
+        <Tag bck="#0e1731" size="50px" color="#ffffff">
+          Matches
+        </Tag>
+
+        {/* BLOCK*/}
+
+        {/* TAG*/}
+        <Tag
+          bck="#ffffff"
+          size="22px"
+          color="#0e1731"
+          link={true}
+          linkto="/the_team"
+        >
+          See More Matches
+        </Tag>
+      </div>
+    </div>
+  );
+};
+
+export default MatchesHome;
